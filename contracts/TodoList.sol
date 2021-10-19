c74f1fa7583fac32ad829c6745b17fcb41f3d31c
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-10-19 12:43:10 +0500

    create task

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -11,6 +11,8 @@ contract TodoList {
 
     mapping(uint => Task) public tasks;
 
+    event TaskCreated(uint id, string content, bool completed);
+
     constructor() public {
         createTask("Task 1");
     }
@@ -18,5 +20,6 @@ contract TodoList {
     function createTask(string memory _content) public {
         taskCount ++;
         tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false)
     }
 }
